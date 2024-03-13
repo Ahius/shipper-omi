51233bd39e71702b36d5ee0ffccc7747b7191ff5
Hieu Doan <hieudoan280102@gmail.com>
2024-03-13 21:52:35 +0700

    Fetch api area done, fix UI, fix config redux

---
+++ src/redux/reducers/areaSlice.js
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchArea = createAsyncThunk('area/fetch', async (_, { rejectWithValue }) => {
+    try {
+        const response = await axios.get('https://onlinemarket-api.nguyenminhhai.us/api/v1/area');
+        return response.data; 
+    } catch (error) {
+        if (error.response) {
+            return rejectWithValue(error.response.data);
+        } else if (error.request) {
+         
+            return rejectWithValue({ message: 'No response received from server.' });
+        } else {
+            
+            return rejectWithValue({ message: 'Error setting up the request.' });
+        }
+    }
+});
+
+
+const areaSlice = createSlice({
+    name: 'areaSlice',
+    initialState: {
+      error: null,
+      loading: false,
+      data: null
+    },
+    reducers: {
+      clearError(state) {
+        state.error = null;
+      }
+    },
+    extraReducers(builder) {
+      builder
+        .addCase(fetchArea.pending, (state) => {
+          state.loading = true;
+        })
+        .addCase(fetchArea.fulfilled, (state, action) => {
+          state.loading = false;
+          state.data = action.payload; 
+          state.error = null;
+        })
+        .addCase(fetchArea.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.payload.message;
+        });
+    }
+  });
+  
+  export const { clearError } = areaSlice.actions;
+  
+  export default areaSlice.reducer;
+  
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,14 @@
-import React, {  useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
-import { login } from '../redux/actions/authSlice';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground, Image } from 'react-native';
 import { useDispatch } from 'react-redux';
+import { login } from '../redux/reducers/authSlice';
+
 const Login = ({ navigation }) => {
     const dispatch = useDispatch();
 
     const [credentials, setCredentials] = useState({ Phone: '', Password: '' });
-    // console.log("data", credentials.Phone, credentials.Password);
     const [error, setError] = useState(null);
+
     const handleRegister = () => {
         navigation.navigate('Register');
     };
@@ -33,87 +34,94 @@ const Login = ({ navigation }) => {
             });
     };
 
-
-
-
     return (
-        <View style={{ flex: 1 }}>
-
-            <View style={styles.topBackground}>
-                <ImageBackground source={{ uri: 'https://i.pinimg.com/.../bfbe1c895b7915a608344511fe13fdba...' }} style={styles.backgroundImage}></ImageBackground>
-            </View>
+        <View style={styles.container}>
+            <ImageBackground source={{ uri: 'https://png.pngtree.com/thumb_back/fw800/back_pic/02/65/88/135788e18a62b79.jpg' }} style={styles.topBackground}>
+                <View style={styles.overlay}></View>
+                <Image source={{ uri: 'https://bambooship.cdn.vccloud.vn/wp-content/uploads/2021/11/shipper-1-1.png' }} style={styles.logo} />
+            </ImageBackground>
             <View style={styles.middleContainer}>
-                <ImageBackground source={{ uri: 'https://i.pinimg.com/.../ebaa506bb2ca7ba89cacb10ec426e96a...' }} style={styles.middleBackground}>
-                    <View style={styles.container}>
-                        <View style={styles.formContainer}>
-                            <Text style={styles.text}>Đăng nhập</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Nhập Số Điện Thoại"
-                                keyboardType="phone-pad" // Change to 'phone-pad' for phone numbers
-                                autoCapitalize="none"
-                                value={credentials.Phone}
-                                onChangeText={(text) => setCredentials({ ...credentials, Phone: text })}
-                            />
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Nhập mật khẩu"
-                                secureTextEntry
-                                value={credentials.Password}
-                                onChangeText={(text) => setCredentials({ ...credentials, Password: text })}
-                            />
-                            <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                                <Text style={styles.buttonText}>Đăng nhập</Text>
-                            </TouchableOpacity>
-                            {error && <Text style={{ color: 'red' }}>{error}</Text>}
-                            <TouchableOpacity onPress={handleRegister}>
-                                <Text>Register</Text>
-                            </TouchableOpacity>
-                            <View style={styles.forgotPasswordContainer}>
-                                <Text style={styles.forgotPasswordText}>Quên mật khẩu?</Text>
-                            </View>
-                        </View>
-                    </View>
-                </ImageBackground>
+                <View style={styles.formContainer}>
+                    <Text style={styles.text}>Đăng nhập</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Nhập Số Điện Thoại"
+                        keyboardType="phone-pad"
+                        autoCapitalize="none"
+                        value={credentials.Phone}
+                        onChangeText={(text) => setCredentials({ ...credentials, Phone: text })}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Nhập mật khẩu"
+                        secureTextEntry
+                        value={credentials.Password}
+                        onChangeText={(text) => setCredentials({ ...credentials, Password: text })}
+                    />
+                    <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                        <Text style={styles.buttonText}>Đăng nhập</Text>
+                    </TouchableOpacity>
+                    {error && <Text style={{ color: 'red', textAlign: 'center' }}>{error}</Text>}
+                    <TouchableOpacity onPress={handleRegister}>
+                        <Text style={{ marginTop: 10, color: '#DA5628', textAlign: 'center' }}>Đăng ký</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.footer}>
+            {/* <View style={styles.footer}>
                 <Text style={styles.footerText}>© 2024 Your App</Text>
-            </View>
+            </View> */}
+            <ImageBackground source={{ uri: 'https://t4.ftcdn.net/jpg/04/19/98/19/360_F_419981971_jftDSPRJGskIgVirQqtKLItcFdEZ4Zve.jpg' }} style={styles.topBackground2}>
+                {/* <View style={styles.overlay}></View> */}
+            </ImageBackground>
         </View>
     );
 };
+
 const styles = StyleSheet.create({
-    topBackground: {
-        flex: 0.1,
-        marginBottom: 10,
-    },
-    middleContainer: {
-        flex: 0.9,
-    },
-    backgroundImage: {
-        flex: 1,
-        resizeMode: 'cover',
-        justifyContent: 'center',
-    },
-    middleBackground: {
-        flex: 1,
-        resizeMode: 'cover',
-        justifyContent: 'center',
-    },
     container: {
         flex: 1,
+        backgroundColor: '#fff',
+    },
+    topBackground: {
+        flex: 0.3,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingHorizontal: 20,
+    },
+
+    topBackground2: {
+        flex: 0.25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderTopLeftRadius: 35, 
+        borderTopRightRadius: 35, 
+        overflow: 'hidden', 
+    },
+    overlay: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0,0.5)', // Điều chỉnh độ mờ và màu sắc của lớp nền
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        borderRadius: 75, 
+    },
+    middleContainer: {
+        flex: 0.6,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     formContainer: {
-        backgroundColor: 'rgba(255, 255, 255, 0.😎',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
         padding: 20,
         borderRadius: 10,
-        borderWidth: 1,
-        borderColor: '#DA5628',
         width: '80%',
         maxWidth: 300,
+    },
+    text: {
+        marginBottom: 30,
+        fontSize: 35,
+        color: '#DA5628',
+        textAlign: 'center'
     },
     input: {
         width: '100%',
@@ -123,12 +131,6 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         paddingHorizontal: 10,
         marginBottom: 10,
-    },
-    text: {
-        marginBottom: 30,
-        fontSize: 35,
-        color: '#DA5628',
-        textAlign: 'center'
     },
     button: {
         width: '100%',
@@ -143,22 +145,15 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 16,
     },
-    forgotPasswordContainer: {
-        position: 'absolute',
-        bottom: 2,
-        right: 20,
-    },
-    forgotPasswordText: {
-        color: '#4B9AD1',
-    },
-    footer: {
-        backgroundColor: '#DA5628',
-        padding: 20,
-        alignItems: 'center',
-    },
-    footerText: {
-        color: 'white',
-        fontSize: 14,
-    },
+    // footer: {
+    //     backgroundColor: '#DA5628',
+    //     padding: 20,
+    //     alignItems: 'center',
+    // },
+    // footerText: {
+    //     color: 'white',
+    //     fontSize: 14,
+    // },
 });
+
 export default Login;
